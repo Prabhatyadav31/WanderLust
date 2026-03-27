11b48e9bed55ecabe1b9f7643f5737b7375dfd89
Prabhatyadav <Prabhatyadav.in@gmail.com>
2026-03-27 22:03:49 +0530

    flash added

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-
 const ExpressError = require("./utils/ExpressError.js");
+const session = require("express-session");
+const flash = require("connect-flash");
+
 
 
 
 const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
+const e = require("express");
+const { maxHeaderSize } = require("http");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -34,8 +37,28 @@ app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
+
+const sessionoptions = {
+  secret: "mysupersecretcode",
+  resave: false,
+  saveUninitialized: true,
+  caches: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000, // 7 days
+    maxAge: 7 * 24 * 60 * 60 * 1000 ,
+    httpOnly: true
+  },
+};
 app.get("/", (req, res) => {
   res.send("Hi, I am root");
+});
+app.use(session(sessionoptions));
+app.use(flash());
+
+// AFTER flash
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
 });
 
 
